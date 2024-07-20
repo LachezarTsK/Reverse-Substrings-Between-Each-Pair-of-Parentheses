b32776ba60eb2c0013448c6497126146b3fccf7c
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-07-20 14:26:29 +0200

    Create Solution.ts

---
+++ Solution.ts
@@ -0,0 +1,41 @@
+
+function reverseParentheses(s: string): string {
+    const input = s.split('');
+    const indexOpeningParentheses = new Array();
+
+    for (let i = 0; i < input.length; ++i) {
+        if (input[i] === '(') {
+            indexOpeningParentheses.push(i);
+            continue;
+        }
+        if (input[i] === ')') {
+            const start = indexOpeningParentheses.pop() + 1;
+            const end = i - 1;
+            reverse(start, end, input);
+        }
+    }
+
+    return createReverseParentheses(input);
+};
+
+function createReverseParentheses(input: string[]): string {
+    const reverseParentheses = new Array();
+    for (let current of input) {
+        if (current !== '(' && current !== ')') {
+            reverseParentheses.push(current);
+        }
+    }
+    return reverseParentheses.join('');
+}
+
+function reverse(start: number, end: number, input: string[]): void {
+    while (start <= end) {
+        swap(start, end, input);
+        ++start;
+        --end;
+    }
+}
+
+function swap(first: number, second: number, input: string[]): void {
+    [input[first], input[second]] = [input[second], input[first]];
+}
